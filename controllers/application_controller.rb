8af3e29d0e84f190975db67193ccfc4c9d0f741b
CarsonBills <CarsonMBills@Gmail.com>
2014-12-04 17:49:04 -0500

    can successfully submit an author's name through params to controller

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < Sinatra::Base
 
   set :views, File.expand_path('../../views', __FILE__)
 
+  get '/' do
+    erb :index
+  end
+
+  post '/write' do
+    binding.pry
+  end
+
 end
